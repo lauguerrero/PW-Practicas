5b4812c2681e61035d86f87f96c25bb2a387bf71
Paco-PE <pacoprietoescobar@gmail.com>
2023-03-13 14:28:48 +0100

    En proceso de añadir la funcionalidad de añadir y eliminar de la lista de deseos

--- pagAnuncios.php
+++ pagAnuncios.php
@@ -42,32 +42,54 @@
                     
                     $logged_user = "1"; #Supuesto ID del usuario loggeado
 
+                    if($_SERVER["REQUEST_METHOD"] == "POST"){
+                        $id_articulo_deseos=$_POST["Id_Articulo"];
+                        $id_usuario_deseos=$_POST["Id_Usuario"];
+                        if($_POST["add-listadeseados"] == 'En tu lista de deseos'){ #En este caso significa que queremos eliminar el archivo de la lista de deseos
+                            $prueba = mysqli_query($conexion, "delete from Deseos where id_Articulo=$id_articulo_deseos and id_Usuario=$id_usuario_deseos");
+                            echo $prueba;
+                        }else{ #En este caso significa que queremos añadir el archivo a la lista de deseos
+                            echo "Queremos add";
+                        }
+                    }
+
+                    #Obtenemos la lista de deseos del usuario loggeado
                     $consulta_deseos = mysqli_query($conexion, "select id_Articulo from Deseos where id_Usuario=$logged_user") or die("Fallo en la consulta de Deseos");
 
+                    #Pasamos los articulos de la lista de deseos del usuario a un array
                     $lista_deseos = array();
                     while($fila = mysqli_fetch_array($consulta_deseos)){
                         $lista_deseos[] = $fila['id_Articulo'];
                     }
                     
+                    echo '<form method="post">';
                     $nfilas = mysqli_num_rows($consulta_articulos);
                     for($i=0; $i<$nfilas; $i++){
                         $fila = mysqli_fetch_array($consulta_articulos);
+
+                        #Hacemos que se hagan 4 columnas
                         if($i%4 == 0){
                             echo '</tr><tr>';
                         }
+
+                        #Introducimos la imagen, el texto y los botones
                         echo '<td><div>';
-                        echo '<div class="add-listadeseados-container"><button class="add-listadeseados">';
+                        echo '<div class="add-listadeseados-container">';
+                        echo '<input type="hidden" name="Id_Articulo" value="'.$fila['Id_Articulo'].'">';
+                        echo '<input type="hidden" name="Id_Usuario" value="'.$logged_user.'">';
+                        echo '<input type="submit" class="add-listadeseados" name="add-listadeseados" ';
                         if(in_array($fila['Id_Articulo'], $lista_deseos)){
-                            echo 'En tu lista de deseos';
+                            echo 'value="En tu lista de deseos"';
                         }else{
-                            echo 'Añadir a tu lista de deseos';
+                            echo 'value="Añadir a tu lista de deseos"';
                         }
-                        echo '</button></div>';
+                        echo '></div>';
                         echo '<a href="#"><img class="anuncio" height="300" width="250" src="../ImagenesArticulos/'.$fila['Imagen'].'.jpg"/></a>'; #Insertamos la imagen con el hipervínculo a la página del anuncio
                         echo '<br>';
                         echo '<a class="texto-anuncio" href="#"><div align="center">'.$fila['Nombre'].' - '.$fila['Precio'].'€</div></a>'; #Insetamos el nombre y el precio con el hipervínculo a la página del anuncio
                         echo '</div></td>';
                     }
+                    echo '</form>';
                 ?>
             </table>
         </div>
